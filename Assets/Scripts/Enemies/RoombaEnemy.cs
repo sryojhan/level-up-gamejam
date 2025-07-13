d85e548a472c079ed351f42c32f243fa23e0ae09
Rogarsu2014 <rogarsu2014@gmail.com>
2025-07-13 17:52:37 +0200

    Changes

--- Assets/Scripts/Enemies/RoombaEnemy.cs
+++ Assets/Scripts/Enemies/RoombaEnemy.cs
@@ -6,15 +6,25 @@ public class RoombaEnemy : BaseEnemy
     {
         base.CustomStart();
         currentState = EnemyState.attack;
-        ownRigidbody.linearVelocity = ((Vector2)target.position - spawnPositionCoordinates) * moveSpeed;
+        ownRigidbody.linearVelocity = ((Vector2)target.position - spawnPositionCoordinates).normalized * moveSpeed;
 
         if(enemyManager)
             enemyManager.AddEnemy();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    public void RoombaCollision(Collision2D collision)
     {
-        Vector2 dir = ((Vector2)target.position - (Vector2)transform.position);
+        Vector2 dir = Vector2.zero;
+
+        if(Vector2.Distance(spawnPositionCoordinates, target.position) > alertRadius)
+        {
+            dir = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1)).normalized;
+        }
+        else
+        {
+            dir = ((Vector2)target.position - (Vector2)transform.position).normalized;
+        }
+
         ownRigidbody.linearVelocity = dir * moveSpeed;
 
         float angle = Mathf.Atan2(dir.y, dir.x) * -Mathf.Rad2Deg;
